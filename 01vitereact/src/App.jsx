da6d18bf1fd0a10a598c06fe81b6be2760ca55b5
Shreya Ganesh Shinde <shreyashinde2256@gmail.com>
2025-08-08 19:30:59 +0530

    Add 01vitereact project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from './chai.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>Chai aur React</h1>
+    <p>This is React js</p>
+    </>
+    
+  )
+}
+
+export default App
